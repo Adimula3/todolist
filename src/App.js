893e9722a1e18a5a8cc72bcb5bb26fd6de4dbaab
Adimula3 <Kolawoleolayinka16@gmail.com>
2022-07-29 07:17:22 -0500

    ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from "react";
 
 function App() {
+    const [todo, setTodo] = useState("");
+    const [todos, setTodos] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (todo !== "")  {
+
+            setTodos([{id:`${todo}-${Date.now()}` , todo}, ...todos])
+            setTodo("");
+        }
+    }
+    const handleDelete = (id) => {
+        const delTodo = todos.filter((to) => to.id !== id)
+        setTodos([...delTodo]);
+    }
+    const handleEdit = () => {
+
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="container">
+            <h1>Todo List App</h1>
+            <form className="todoForm" onSubmit={handleSubmit}>
+                <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)}/>
+                <button type="submit">Go</button>
+            </form>
+
+            <ul className="allTodos">
+                {todos.map(((t) =>
+                        <li className="singleTodos"><span className="todoText" key={t.id}>{t.todo}</span>
+                            <button onClick={() => handleEdit(t.id)}>Edit</button>
+                            <button onClick={() => handleDelete(t.id)}>Delete</button>
+                        </li>
+                    ))}
+
+            </ul>
+        </div>
     </div>
   );
 }
